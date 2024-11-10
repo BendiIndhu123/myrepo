1ae5699e42224b2f320f3dffa504c487b7d01b99
BendiIndhu123 <bendiindhu2004@gmail.com>
2024-11-10 10:39:34 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
-class Test
-  {
-    public static void main(String[] args)
-    {
-      System.out.println("Welcome Devops")
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+}  
